7e0e7dc7303e3c07d37925a9165e9e283a9c8264
awfacai <aw168888888@outlook.com>
2025-04-06 23:09:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,12 +3,7 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  // 获取原始请求的 URL
   const url = new URL(request.url)
-  
-  // 构建目标 URL，保留路径和查询参数
   const targetURL = 'https://aw168-awql.hf.space' + url.pathname + url.search
-  
-  // 返回 301 永久重定向
   return Response.redirect(targetURL, 301)
 }
