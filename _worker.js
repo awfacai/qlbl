2857c1db4e10d00cd08005f3771155efd97409c5
awfacai <aw168888888@outlook.com>
2025-04-06 23:14:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,5 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const targetURL = 'https://aw168-awql.hf.space' + url.pathname + url.search
-  return Response.redirect(targetURL, 301)
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  const targetURL = 'https://aw168-awql.hf.space' + url.pathname + url.search;
+  return Response.redirect(targetURL, 301); // 301 为永久重定向
 }
